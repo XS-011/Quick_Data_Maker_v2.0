552bee0367c09d9b0747fdddd44a2f6d99ce92f9
XS-011 <50036378+XS-011@users.noreply.github.com>
2020-02-22 09:00:09 +0800

    Update numbermaker.cpp

--- numbermaker.cpp
+++ numbermaker.cpp
@@ -10,7 +10,7 @@ struct node
 {
 	char Names[25];
 	int Kinds;//变量种类1整数型 2.字符型 3.浮点型
-	char Min,Max;
+	char Min[25],Max[25];
 	int Group;//1表明他是数组，2表明不是
 	char Times[35];
 	int After;//0为之后不输出任何东西，1为空格，2为换行
@@ -87,9 +87,9 @@ void Make_New()
 	if(variable[variable_n].Kinds==1)
 	{
 		printf("这个变量的最大值是多少？\n");
-		cin>>variable[variable_n].Min;
-		printf("这个变量的最小值是多少？\n");
 		cin>>variable[variable_n].Max;
+		printf("这个变量的最小值是多少？\n");
+		cin>>variable[variable_n].Min;
 		if(variable[variable_n].Group==1)
 		{
 			printf("这个变量输出多少次？\n");
@@ -112,37 +112,117 @@ void Working()
 	FILE *k;
 	k=fopen(".//data//Runthis.cpp","w+");
 	fprintf(k,"#include <bits/stdc++.h>\n");
-	fprintf("#define ll long long");
+	fprintf(k,"#define ll long long\n");
 	fprintf(k,"using namespace std;\n");
-	fprintf(k,"char Modle1=%cabcdefghijklmnopqrstuvwxyz%c",'"','"');
+	fprintf(k,"char Modle1[100]=%cabcdefghijklmnopqrstuvwxyz%c;\n",'"','"');
 	for(int i=1;i<=variable_n;i++)
 	{
 		if(variable[variable_n].Kinds==1)
-		fprintf(k,"ll %s;\n"variable[i].Names);
+		fprintf(k,"ll %s;\n",variable[i].Names);
 	}
-	fprintf(k,"ll RP()\n{return (ll)(rand()*(ll)(rand()*(ll)(rand()*(ll)(rand());}\n");
+	fprintf(k,"ll RP()\n{return (ll)((ll)rand()*(ll)rand()*(ll)rand()*(ll)rand());}\n");
 	fprintf(k,"signed main()\n");
-	printf("{\n");
-	printf("srand(0)");
+	fprintf(k,"{\n");
+	fprintf(k,"srand(0);\n");
 	for(int i=Start;i<=End;i++)
 	{
 		fprintf(k,"FILE *Act%d;\n",i);
-		fprintf(k,"Act%d=fopen(%c%d.in%c);\n",i,'"',i,'"');
+		fprintf(k,"Act%d=fopen(%c%d.in%c,%cw+%c);\n",i,'"',i,'"','"','"');
 		for(int j=1;j<=variable_n;j++)
 		{
 			if(variable[j].Group==1)
 			fprintf(k,"for(int i=1;i<=%s;i++)\n{\n",variable[j].Times);
+			
+			if(variable[j].Kinds==1)
+			{
+				fprintf(k,"%s=(ll)(%s)+(ll)( RP()%((ll)(%s-%s+1)) );\n",variable[j].Names,variable[j].Min,variable[j].Max,variable[j].Min);
+				fprintf(k,"fprintf(Act%d,%c",i,'"');
 
-			fprintf(k,"%s=(ll)(%s)+(ll)( RP()%((ll)(%s-%s+1)) );\n",variable[j].Names,variable[j].Min,variable[j].Max,variable[j].Min);
-			fprintf("fprintf(Act%d,%c",i,'"');
+			
+				fprintf(k,"%c%c%c%c",'%','l','l','d');
+			
+				if(variable[j].After==1)//0.什么都没有 1.空格 2.换行
+				fprintf(k," ");
+				if(variable[j].After==2)
+				fprintf(k,"%c%c",92,'n');
 
-			if(variable[j].Kinds)
+				fprintf(k,"%c,%s);\n",'"',variable[j].Names);
+
+				if(variable[j].Group==1)
+				fprintf(k,"}\n",variable[j].Times);
+			}
 		}
 	}
+	fprintf(k,"return 0;");
+	fprintf(k,"}");
+	exit(0);
 }
 void Check_All()
 {
 	Working();
+}
+void Check(int ppp)
+{
+	int q=1;
+	while(1)
+	{
+		system("cls");
+		if(variable[ppp].Kinds=='d')
+		{
+			for(int i=1;i<=8;i++)
+			printf(" ");
+			if(q==1)
+			printf("->");
+			else
+			printf("  ");
+			printf("【变量名】：%s",variable[ppp].Names);
+			if(q==1)
+			printf("<-");
+			printf("\n");
+
+			for(int i=1;i<=8;i++)
+			printf(" ");
+			if(q==2)
+			printf("->");
+			else
+			printf("  ");
+			printf("【最大值】：%d",variable[ppp].Max);
+			if(q==2)
+			printf("<-");
+			printf("\n");
+
+			for(int i=1;i<=8;i++)
+			printf(" ");
+			if(q==3)
+			printf("->");
+			else
+			printf("  ");
+			printf("【最小值】：%d",variable[ppp].Min);
+			if(q==3)
+			printf("<-");
+			printf("\n");
+
+			for(int i=1;i<=8;i++)
+			printf(" ");
+			if(q==4)
+			printf("->");
+			else
+			printf("  ");
+			printf("【之后输出】：");
+			if(variable[ppp].After==0)
+			printf("无");
+			else if(variable[ppp].After==1)
+			printf("空格");
+			else
+			printf("换行");
+			if(q==4)
+			printf("<-");
+			printf("\n");
+			
+			
+
+		}
+	}
 }
 void main_page()
 {
@@ -183,7 +263,7 @@ void main_page()
 			putchar(' ');
 		}
 		printf("变量：【%s】",variable[i].Names);
-		if(h==i+1)
+		if(h==i)
 		{
 			putchar('<');
 			putchar('-');
